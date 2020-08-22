2977bb4bcf80c15d7c534fb6b582a5dfb1a974fc
Myriam <wicnmys@gmail.com>
2020-08-22 12:05:15 +0300

    separated extraction and unzipping

--- setup-env.py
+++ setup-env.py
@@ -1,5 +1,6 @@
 from io import BytesIO
-from urllib.request import urlopen
+#from urllib.request import urlopen
+from urllib.request import urlretrieve
 from zipfile import ZipFile
 import os
 import json
@@ -14,15 +15,30 @@ def clean_foldername(address):
   foldername = split_address[-1].split('.')
   return foldername[0]
 
+def clean_filename(address):
+  split_address = address.split('/')
+  filename = split_address[-1].split('?')
+  return filename[0]
 
-def download_file(url,target):
+def download_file(url, target):
+  filename = clean_filename(url)
+  if filename not in os.listdir(target):
+    print('Downloading ' + filename)
+    urlretrieve(url, target + '/' + filename)
+  else:
+    print(filename + ' already downloaded.')
+
+  return target + '/' + filename
+
+def extract_file(file, target):
     # Download archive
     try:
-      with urlopen(url) as zipresp:
-        with ZipFile(BytesIO(zipresp.read())) as zfile:
-          zfile.extractall(target)
+      with ZipFile(file) as zfile:
+        print('Unzipping ' + file)
+        zfile.extractall(target)
 
     except Exception as e:
+      print('Failed to unzip ' + file)
       print(e)
       return 1
 
@@ -33,11 +49,8 @@ def dl_unzip(address,target):
     os.makedirs(safe_target)
     print("Creating folder " + target + " in " + os.getcwd())
 
-  if not os.path.isdir(foldername):
-    print("Downloading and unpacking " + address + " to " + foldername)
-    download_file(address,foldername)
-  else:
-    print(foldername + " already exists.")
+  file_path = download_file(address, target)
+  extract_file(file_path,target)
 
 if __name__ == "__main__":
 
