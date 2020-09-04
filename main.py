492e1c71af0325c6e1261cb8f692a72d03e6be32
Myriam <wicnmys@gmail.com>
2020-09-04 19:25:25 +0300

    minor changes

--- main.py
+++ main.py
@@ -3,5 +3,4 @@ from camerapose import CameraPose
 if __name__ == "__main__":
     model = CameraPose("config.json")
     #model.train()
-    test = model.test(r"/home/myriam/projects/compv-final/output/f85f48deeeb411eab8f7ccb0da67463d")
-    #print(test)
+    model.test()
